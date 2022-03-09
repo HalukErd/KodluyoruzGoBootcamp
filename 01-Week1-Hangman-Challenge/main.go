460f84cb88e1ca8820bbb67d12329a3a2e81cdb6
halukerd <erd.haluk@gmail.com>
2022-03-09 19:56:34 +0300

    Typo is fixed

--- 01-Week1-Hangman-Challenge/main.go
+++ 01-Week1-Hangman-Challenge/main.go
@@ -118,7 +118,7 @@ func clear() {
 	if runtime.GOOS == "windows" {
 		cmd = exec.Command("cmd", "/c", "cls")
 	} else {
-		cmd = exec.Command("clear2")
+		cmd = exec.Command("clear")
 	}
 	cmd.Stdout = os.Stdout
 	cmd.Run()
